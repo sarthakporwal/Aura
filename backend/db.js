5fc2b877028f14cadf6750dd3db012b9a74dffd1
Parth Kumar <127124317+27-Parth@users.noreply.github.com>
2025-02-20 14:46:37 +0530

    Update db.js
    Changed the existing MongoDB databse URL

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
-const mongoURI = "mongodb://127.0.0.1:27017/priyanshi";
+const mongoose = require('mongoose'); //schema file
+const mongoURI = "mongodb://127.0.0.1:27017/parth2704"; 
 
 const connectMongo = () => { // connecting the mongoDB
     mongoose.connect(mongoURI)
     .then(() => {
-        console.log('mongoDB connected');
+        console.log('Database Connected');
     })
     .catch((err) => {
         console.log("error: " + err);
